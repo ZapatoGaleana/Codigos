846b68e581f7f34682b203596dc337627d100d4d
ZapatoGaleana <20243ds074@utez.edu.mx>
2025-05-22 19:15:03 -0600

    se me olvido agreagar algo xd

--- Mi_Trabajo_Java/src/principal.java
+++ Mi_Trabajo_Java/src/principal.java
@@ -8,20 +8,20 @@ public class principal {
 
         cuadrado cua = new cuadrado();
         cua.setLado(10);
-        System.out.printf("El perímetro es: %.2f\n", cua.getPerimeter());
-        System.out.printf("El área del cuadrado es: %.2f\n", cua.getArea());
+        System.out.printf("El perimetro es: %.2f\n", cua.getPerimeter());
+        System.out.printf("El area del cuadrado es: %.2f\n", cua.getArea());
 
         circulo cir = new circulo();
         cir.setRadio(10);
-       System.out.printf("El perímetro del círculo es: %.2f\n", cir.getPerimeter2());
-        System.out.printf("El área del círculo es: %.2f\n", cir.getArea2());
+       System.out.printf("El perimetro del circulo es: %.2f\n", cir.getPerimeter2());
+        System.out.printf("El area del circulo es: %.2f\n", cir.getArea2());
 
         tringulo tri = new tringulo();
         tri.setLado2(10);
         tri.setBase(10);
         tri.setAltura(10);
-        System.out.printf("El perímetro del triángulo es: %.2f\n", tri.getPerimeter3());
-        System.out.printf("El área del triángulo es: %.2f\n", tri.getArea3());
+        System.out.printf("El perimetro del triangulo es: %.2f\n", tri.getPerimeter3());
+        System.out.printf("El area del triangulo es: %.2f\n", tri.getArea3());
 
     }
 }
