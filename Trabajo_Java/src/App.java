1be80d756d469c36cf5e47c31c321be3c507ab99
ZapatoGaleana <20243ds074@utez.edu.mx>
2025-05-25 15:23:12 -0600

    Modificacion del archivo estudiantes

--- Trabajo_Java/src/App.java
+++ Trabajo_Java/src/App.java
@@ -9,26 +9,35 @@ public class App
         //Digo para que no piense que lo hice con chat jsjsjs
         // Es que la otra vez vi que algunos ya ponian eso de pedir los valores.
         Scanner scanner = new Scanner(System.in);
-        Estudiante estudiante = new Estudiante();
+        Estudiante[] estudiante = new Estudiante[5];
+
+        for (int i=0; i<5; i++)
+        {
+        System.out.println("======= Estudiante " + (i+1) + " =======");
+        estudiante[i] = new Estudiante();
 
         System.out.println("Ingrese el nombre del estudiante:");
         String nombre = scanner.nextLine();
-        estudiante.setNombre(nombre);
+        estudiante[i].setNombre(nombre);
         System.out.println("Ingrese la matricula del estudiante:");
         String matricula = scanner.nextLine();
-        estudiante.setMatricula(matricula);
+        estudiante[i].setMatricula(matricula);
 
+        for (int a = 0; a < 5; a++) 
+        {
+            System.out.println("Ingrese la calificacion " + (a + 1));
+            double nota = scanner.nextDouble();
+            estudiante[i].setNota(a,nota);
+        }
+         scanner.nextLine();
+        }
+       
         for (int i = 0; i < 5; i++) 
         {
-            System.out.println("Ingrese la calificacion " + (i + 1));
-            double nota = scanner.nextDouble();
-            estudiante.setNota(i, nota);
-        }
-
-        System.out.println("Nombre: " + estudiante.getNombre());
-        System.out.println("Matricula: "+ estudiante.getMatricula());
-        System.out.println("Promedio: " + estudiante.calcularPromedio());
-        if (estudiante.aprobado()) 
+        System.out.println("Nombre: " + estudiante[i].getNombre());
+        System.out.println("Matricula: "+ estudiante[i].getMatricula());
+        System.out.println("Promedio: " + estudiante[i].calcularPromedio());
+        if (estudiante[i].aprobado()) 
         {
             System.out.println("Aprobado?: Si");
         } 
@@ -36,6 +45,8 @@ public class App
         {
             System.out.println("Aprobado?: No");
         }
+        }
+        System.out.println();
         scanner.close();
     }
 }
